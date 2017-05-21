65312465866c1fb99663ae29471a15c8643373b7
Adnan Akhundov <adnan.akhundov@gmail.com>
2017-05-21 13:27:41 +0200

    All data generation tools and tests have been moved to a separate data.py file.

---
+++ data.py
@@ -0,0 +1,273 @@
+import numpy as np
+
+
+logic_gates = [
+    [[1, 0], [0, 0]],
+    [[0, 1], [0, 0]],
+    [[0, 0], [1, 0]],
+    [[0, 1], [1, 0]],
+    [[1, 1], [1, 0]],
+    [[0, 0], [0, 1]],
+    [[1, 0], [0, 1]],
+    [[1, 1], [0, 1]],
+    [[1, 0], [1, 1]],
+    [[0, 1], [1, 1]]
+]
+
+
+def generate_parity_data(batch_size, dimensions=64, seed=None):
+    inputs, targets = [], []
+
+    if seed is not None:
+        np.random.seed(seed)
+
+    for b in range(batch_size):
+        vector = np.zeros([dimensions])
+        idx = np.arange(0, dimensions)
+        np.random.shuffle(idx)
+
+        threshold = np.random.randint(1, dimensions + 1)
+        values = np.random.randint(2, size=[threshold]) * 2 - 1
+        vector[idx[:threshold]] = values
+
+        inputs.append([vector])
+        targets.append([[
+            np.sum(np.where(
+                values > 0, [1.0], [0.0]
+            )) % 2
+        ]])
+
+    if seed is not None:
+        np.random.seed()
+
+    return np.stack(inputs), np.stack(targets)
+
+
+def generate_logic_data(batch_size, min_time_steps=1, max_time_steps=10, max_gates=10, seed=None):
+    total_gates = len(logic_gates)
+    inputs, targets, seq_length = [], [], []
+
+    if seed is not None:
+        np.random.seed(seed)
+
+    for b in range(batch_size):
+        input_steps = np.zeros([max_time_steps, max_gates * total_gates + 2])
+        target_steps = np.zeros([max_time_steps, 1])
+
+        seq_length.append(
+            np.random.randint(
+                min_time_steps,
+                max_time_steps + 1
+            )
+        )
+
+        b0 = np.random.randint(2)
+        for t in range(seq_length[-1]):
+            b1 = np.random.randint(2)
+            if t == 0:
+                input_steps[t][0] = b0
+            input_steps[t][1] = b1
+
+            num_gates = np.random.randint(1, max_gates + 1)
+
+            for g in range(num_gates):
+                gate = np.random.randint(total_gates)
+                b1, b0 = logic_gates[gate][b1][b0], b1
+                input_steps[t][2 + g * total_gates + gate] = 1
+
+            target_steps[t][0] = b1
+            b0 = b1
+
+        inputs.append(input_steps)
+        targets.append(target_steps)
+
+    if seed is not None:
+        np.random.seed()
+
+    return np.stack(inputs), np.stack(targets), seq_length
+
+
+def generate_addition_data(batch_size, min_time_steps=1, max_time_steps=5, max_digits=5, seed=None):
+    inputs, targets, seq_length = [], [], []
+
+    if seed is not None:
+        np.random.seed(seed)
+
+    for b in range(batch_size):
+        input_steps = np.zeros([max_time_steps, max_digits * 10])
+        target_steps = np.zeros([max_time_steps, max_digits+1, 11])
+
+        seq_length.append(
+            np.random.randint(
+                min_time_steps,
+                max_time_steps + 1
+            )
+        )
+
+        running_sum = 0
+        for t in range(seq_length[-1]):
+            digits_no = np.random.randint(1, max_digits + 1)
+            current_number = np.random.randint(10 ** (digits_no - 1), 10 ** digits_no)
+            number_digits = np.array([int(c) for c in str(current_number)])
+
+            for d in range(digits_no):
+                input_steps[t][d * 10 + number_digits[d]] = 1
+
+            running_sum += current_number
+            sum_digits = np.array([int(c) for c in str(running_sum)])
+
+            for d in range(max_digits + 1):
+                if d < len(sum_digits):
+                    target_steps[t][d][sum_digits[d]] = 1
+                else:
+                    target_steps[t][d][10] = 1
+
+        inputs.append(input_steps)
+        targets.append(target_steps)
+
+    if seed is not None:
+        np.random.seed()
+
+    return np.stack(inputs), np.stack(targets), seq_length
+
+
+def generate_sort_data(batch_size, min_numbers=2, max_numbers=15, seed=None):
+    inputs, targets, seq_length = [], [], []
+
+    if seed is not None:
+        np.random.seed(seed)
+
+    for b in range(batch_size):
+        input_steps = np.zeros([max_numbers * 2, 2])
+        target_steps = np.zeros([max_numbers * 2, max_numbers])
+
+        numbers_count = np.random.randint(
+            min_numbers,
+            max_numbers + 1
+        )
+        seq_length.append(numbers_count * 2)
+        numbers = np.random.randn(numbers_count)
+        indices = np.argsort(numbers)
+
+        for n in range(numbers_count * 2):
+            if n < numbers_count:
+                input_steps[n][1] = numbers[n]
+                if n == numbers_count - 1:
+                    input_steps[n][0] = 1
+            else:
+                target_steps[n][indices[n - numbers_count]] = 1
+
+        inputs.append(input_steps)
+        targets.append(target_steps)
+
+    if seed is not None:
+        np.random.seed()
+
+    return np.stack(inputs), np.stack(targets), seq_length
+
+
+def test_parity_data(inputs, targets):
+    for b in range(len(inputs)):
+        computed_parity = 0.0
+        for d in range(len(inputs[b][0])):
+            if inputs[b][0][d] == 1.0:
+                computed_parity = 1 - computed_parity
+
+        target_parity = int(targets[b][0][0])
+        assert (computed_parity == target_parity),\
+            "Parity does not match at batch {0}: {1} (computed) vs. {2} (target)".format(
+                    b, computed_parity, target_parity
+                )
+
+
+def test_logic_data(inputs, targets, seq_length):
+    gates_num = len(logic_gates)
+    for b in range(len(inputs)):
+        for t in range(seq_length[b]):
+            b0 = int(inputs[b][0][0]) if t == 0 else b1
+            b1 = int(inputs[b][t][1])
+
+            if t > 0:
+                assert (inputs[b][t][0] == 0.0), \
+                    "First bit is not zero in batch {0} time-step {1}".format(b, t)
+
+            for g in range((len(inputs[b][t]) - 2) // gates_num):
+                one_hot = inputs[b][t][g * gates_num + 2:(g+1) * gates_num + 2]
+                if np.max(one_hot) == 1.0:
+                    gate = np.argmax(one_hot)
+                    b1, b0 = logic_gates[gate][b1][b0], b1
+
+            target_bit = int(targets[b][t])
+            assert (target_bit == b1), \
+                "Target bit does not match at batch {0} time step {1}: {2} (computed) vs. {3} (target)".format(
+                    b, t, b1, target_bit
+                )
+
+
+def test_addition_data(inputs, targets, seq_length):
+    for b in range(len(inputs)):
+        running_sum = 0
+        for t in range(seq_length[b]):
+            current_number = 0
+            for d in range(len(inputs[b][t]) // 10):
+                one_hot = inputs[b][t][d * 10:d * 10 + 10]
+                if np.max(one_hot) == 1.0:
+                    current_number = current_number * 10 + np.argmax(one_hot)
+
+            target_sum = 0
+            for d in range(len(targets[b][t])):
+                one_hot = targets[b][t][d]
+                if one_hot[-1] == 0.0:
+                    target_sum = target_sum * 10 + np.argmax(one_hot)
+
+            running_sum += current_number
+            assert (running_sum == target_sum), \
+                "Running sum doesn't match at batch {0} time step {1}: {2} (computed) vs. {3} (target)".format(
+                    b, t, running_sum, target_sum
+                )
+
+
+def test_sort_data(inputs, targets, seq_length):
+    for b in range(len(inputs)):
+        numbers, target_indices = [], []
+        numbers_count = seq_length[b] // 2
+
+        for n in range(numbers_count):
+            numbers.append(inputs[b][n][1])
+            target_indices.append(
+                np.argmax(targets[b][numbers_count + n])
+            )
+
+            if n == numbers_count - 1:
+                assert (inputs[b][n][0] == 1.0), "End bit is not one at batch {0} time step {1}".format(b, n)
+            else:
+                assert (inputs[b][n][0] == 0.0), "End bit is not zero at batch {0} time step {1}".format(b, n)
+
+        computed_indices = np.argsort(numbers)
+        assert np.all(computed_indices == target_indices), \
+            "Sorted indices don't match at batch {0}: {1} (computed) vs. {2} (target)".format(
+                b, computed_indices, target_indices
+            )
+
+
+if __name__ == "__main__":
+
+    print("Testing parity data... ", end="")
+    for i in range(100):
+        test_parity_data(*generate_parity_data(128))
+    print("passed")
+
+    print("Testing logic data... ", end="")
+    for i in range(100):
+        test_logic_data(*generate_logic_data(16))
+    print("passed")
+
+    print("Testing addition data... ", end="")
+    for i in range(100):
+        test_addition_data(*generate_addition_data(32))
+    print("passed")
+
+    print("Testing sort data... ", end="")
+    for i in range(100):
+        test_sort_data(*generate_sort_data(16))
+    print("passed")
