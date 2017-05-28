96e0cf62e05d4db34d81082ac50f1d7b180fc29c
Adnan Akhundov <adnan.akhundov@gmail.com>
2017-05-28 22:33:33 +0200

    Parity training has been refactored and stabilized. ACTModel is now used for model construction. Model saving and logging functionality has been added.

--- train_parity.py
+++ train_parity.py
@@ -3,107 +3,137 @@ import tensorflow as tf
 import tensorflow.contrib.rnn as rnn
 
 from act_wrapper import ACTWrapper
+from act_model import ACTModel
+
 from data import generate_parity_data
 
 
-TRAIN_STEPS = 1000000
+def echo(message, file):
+    print(message)
+    file.write(message + "\n")
+
+
+SEED = 0
+TRAIN_STEPS = 250000
 BATCH_SIZE = 128
 VAL_SIZE = 1000
 
+DIMENSIONS = 16
 TIME_STEPS = 1
-INPUT_SIZE = 64
-TARGET_SIZE = 2
+INPUT_SIZE = DIMENSIONS
+NUM_CLASSES = 2
 
 NUM_HIDDEN = 128
-TIME_PENALTY = 0.001
-LEARNING_RATE = 0.0001
+TIME_PENALTY = 0.1
+PONDER_LIMIT = 20
+LEARNING_RATE = 0.001
+WITH_ACT = False
 
 
-# setting up RNN cells
-cell = rnn.BasicRNNCell(NUM_HIDDEN)
-cell = ACTWrapper(cell, ponder_limit=100)
+if __name__ == "__main__":
 
-# setting up placeholders
-xs = tf.placeholder(tf.float32, [None, TIME_STEPS, INPUT_SIZE])
-ys = tf.placeholder(tf.float32, [None, TARGET_SIZE])
+    # parsing command line
+    # arguments should be here
 
-# creating RNN with static_rnn()
-print("Creating network...")
-rnn_outputs, rnn_state = rnn.static_rnn(
-    cell, tf.unstack(xs, axis=1), dtype=tf.float32
-)
+    model_name = "{0}_{1}_{2}".format(
+        "parity",
+        TIME_PENALTY if WITH_ACT else "x",
+        SEED
+    )
 
-# inference artifacts
-out_weights = tf.Variable(tf.truncated_normal([NUM_HIDDEN, TARGET_SIZE], stddev=0.01), name="out_weights")
-out_bias = tf.Variable(tf.constant(0.1, shape=[TARGET_SIZE]), name="out_biases")
-logits = tf.matmul(rnn_outputs[-1], out_weights) + out_bias
+    np.random.seed(SEED)
+    tf.set_random_seed(SEED)
 
-# evaluation artifacts
-mistakes = tf.not_equal(tf.argmax(ys, 1), tf.argmax(logits, 1))
-error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
-
-# training artifacts
-softmax_loss = tf.losses.softmax_cross_entropy(ys, logits)
-if isinstance(cell, ACTWrapper):
-    ponder_loss = TIME_PENALTY * cell.ponder_cost
-    total_loss = softmax_loss + ponder_loss
-else:
-    total_loss = softmax_loss
-optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
-print("Computing gradients...")
-train = optimizer.minimize(total_loss)
-
-
-with tf.Session() as sess:
-    print("Initializing variables...")
-    sess.run(tf.global_variables_initializer())
-
-    print("Training...")
+    print(model_name)
+    print()
+    print("dimensions", DIMENSIONS)
+    print("time penalty", TIME_PENALTY)
+    print("ponder limit", PONDER_LIMIT)
+    print("learning rate", LEARNING_RATE)
+    print("with ACT" if WITH_ACT else "without ACT")
     print()
 
-    val_xs, val_ys = generate_parity_data(VAL_SIZE, dimensions=INPUT_SIZE, seed=1)
+    cell = rnn.BasicRNNCell(NUM_HIDDEN)
+    if WITH_ACT:
+        cell = ACTWrapper(cell, ponder_limit=PONDER_LIMIT)
 
-    for step in range(TRAIN_STEPS):
-        batch_xs, batch_ys = generate_parity_data(BATCH_SIZE, dimensions=INPUT_SIZE)
+    inputs = tf.placeholder(tf.float32, [None, TIME_STEPS, INPUT_SIZE])
+    targets = tf.placeholder(tf.int64, [None, TIME_STEPS])
 
-        sess.run(train, feed_dict={
-            xs: batch_xs,
-            ys: batch_ys[:, 0, :]
-        })
+    print("Creating model...")
+    model = ACTModel(
+        inputs, targets, TIME_STEPS, NUM_CLASSES, cell, TIME_PENALTY,
+        optimizer=tf.train.AdamOptimizer(LEARNING_RATE)
+    )
 
-        if (step + 1) % 1000 == 0:
-            if isinstance(cell, ACTWrapper):
-                val_error, val_soft_loss, val_pond_loss, val_ponder = sess.run(
-                    [error, softmax_loss, ponder_loss, cell.ponder_steps],
-                    feed_dict={
-                        xs: val_xs,
-                        ys: val_ys[:, 0, :]
-                    }
-                )
+    log_path = "./results/logs/" + model_name + ".txt"
+    model_path = "./results/models/" + model_name + ".ckpt"
 
-                print(("steps {:2d}  val error {:3.2f}%  soft loss {:.6}  pond loss {:.6}  " +
-                       "min {:.2f}  avg {:.2f}  std {:.2f}  max {:.2f}").format(
-                        step + 1,
-                        100 * val_error,
-                        val_soft_loss,
-                        val_pond_loss,
-                        np.min(val_ponder),
-                        np.average(val_ponder),
-                        np.std(val_ponder),
-                        np.max(val_ponder)
+    saver = tf.train.Saver()
+    log = open(log_path, "w")
+
+    with tf.Session() as sess:
+        print("Initializing variables...")
+        sess.run(tf.global_variables_initializer())
+
+        print("Training...")
+        print()
+
+        if WITH_ACT:
+            echo("{:10}{:<10}{:<15}{:<17}{:<30}".format(
+                "steps", "error",
+                "soft. loss", "pond. loss",
+                "min / avg / std / max ponder"
+            ), log)
+            echo("-" * 80, log)
+        else:
+            echo("{:10}{:<10}{:<15}".format(
+                "steps", "error", "soft. loss"
+            ), log)
+            echo("-" * 30, log)
+
+        val_xs, val_ys = generate_parity_data(VAL_SIZE, dimensions=DIMENSIONS)
+
+        for step in range(TRAIN_STEPS):
+            batch_xs, batch_ys = generate_parity_data(BATCH_SIZE, dimensions=DIMENSIONS)
+
+            sess.run(model.training, feed_dict={
+                inputs: batch_xs,
+                targets: batch_ys
+            })
+
+            if (step + 1) % 1000 == 0:
+                if WITH_ACT:
+                    val_error, val_soft_loss, val_pond_loss, val_ponder = sess.run(
+                        [model.evaluation, model.softmax_loss, model.ponder_loss, model.ponder_steps],
+                        feed_dict={
+                            inputs: val_xs,
+                            targets: val_ys
+                        }
                     )
-                )
-            else:
-                val_error, val_loss = sess.run(
-                    [error, total_loss],
-                    feed_dict={
-                        xs: val_xs,
-                        ys: val_ys[:, 0, :]
-                    }
-                )
 
-                print("steps {:2d}  val error {:3.2f}%  val loss {:.6f}".format(
-                    step + 1,
-                    100 * val_error,
-                    val_loss
-                ))
+                    echo("{:<10d}{:<10.2f}{:<15.6}{:<17.6}{:<30}".format(
+                        step + 1, 100 * val_error,
+                        val_soft_loss, val_pond_loss,
+                        "{:.2f} / {:.2f} / {:.2f} / {:.2f}".format(
+                            np.min(val_ponder), np.mean(val_ponder),
+                            np.std(val_ponder), np.max(val_ponder)
+                        )
+                    ), log)
+                else:
+                    val_error, val_loss = sess.run(
+                        [model.evaluation, model.softmax_loss],
+                        feed_dict={
+                            inputs: val_xs,
+                            targets: val_ys
+                        }
+                    )
+
+                    echo("{:<10d}{:<10.2f}{:<15.6}".format(
+                        step + 1, 100 * val_error, val_loss
+                    ), log)
+
+        print()
+        print("Saving model...")
+        saver.save(sess, model_path)
+        log.close()
